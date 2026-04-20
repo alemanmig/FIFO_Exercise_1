6f80d77a27431536749970fd9d2f4429cbcc504e
Miguel <alemanmig@gmail.com>
2026-04-20 11:10:53 -0700

    feat: update config.sv file

--- verification/directed/sv/config_pkg.sv
+++ verification/directed/sv/config_pkg.sv
@@ -7,6 +7,11 @@ package config_pkg;
 
   localparam int unsigned ClkFreq       = 100_000_000; // 100 MHz
   localparam int unsigned StableTime    = 1;           // 1 ms
+  
+  localparam int DEPTH               = 8;
+  localparam int WIDTH               = 8;
+  localparam int ALMOST_FULL_THRESH  = 6;
+  localparam int ALMOST_EMPTY_THRESH = 2;
 
   int unsigned debounce_time = 120;         // Clock cycles
   int unsigned iterations    = 50;        // Number of bounces
